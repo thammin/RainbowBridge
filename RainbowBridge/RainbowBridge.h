fe6c25c52d4edf3bf28f42d1a5aae3f9ac4b658d
thammin <rim_paulyoung@cyberagent.co.jp>
2015-09-11 11:19:27 +0900

    Initial Commit

---
+++ RainbowBridge/RainbowBridge.h
@@ -0,0 +1,19 @@
+//
+//  RainbowBridge.h
+//  RainbowBridge
+//
+//  Created by 林 柏楊 on 2015/09/11.
+//  Copyright © 2015年 林 柏楊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RainbowBridge.
+FOUNDATION_EXPORT double RainbowBridgeVersionNumber;
+
+//! Project version string for RainbowBridge.
+FOUNDATION_EXPORT const unsigned char RainbowBridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RainbowBridge/PublicHeader.h>
+
+
